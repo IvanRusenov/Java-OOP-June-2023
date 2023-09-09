7d8b174f82ffb2a308940ccc7ec8a0805202550c
IvanRusenov <i_rusenov@abv.bg>
2023-09-09 09:25:56 +0200

    Create CardRank.java

---
+++ WorkingWithAbstractionExercise/T03CardsWithPower/CardRank.java
@@ -0,0 +1,37 @@
+package WorkingWithAbstractionExercise.T03CardsWithPower;
+
+public enum CardRank {
+    ACE(14),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13);
+
+    private int power;
+
+    CardRank(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public static void print(){
+        System.out.println("Card Ranks:");
+
+        for (CardRank cardRank : CardRank.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s\n", cardRank.ordinal(), cardRank.name());
+        }
+    }
+
+
+}
