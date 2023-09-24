ac0030ac3058baae2887f320615deb040478a77c
IvanRusenov <i_rusenov@abv.bg>
2023-09-24 08:56:23 +0200

    Create Main.java

---
+++ InheritanceLab/StackOfStrings/Main.java
@@ -0,0 +1,20 @@
+package InheritanceLab04StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StackOfStrings sos = new StackOfStrings();
+
+        sos.push("One");
+        sos.push("Two");
+        sos.push("Three");
+
+        System.out.println(sos.isEmpty());
+        System.out.println(sos.peek());
+
+        System.out.println(sos.pop());
+        System.out.println(sos.pop());
+        System.out.println(sos.pop());
+
+    }
+}
