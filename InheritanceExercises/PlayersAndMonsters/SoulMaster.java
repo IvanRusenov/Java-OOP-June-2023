ecdef2e597664b5039eb3727326c1954dc6f3888
IvanRusenov <i_rusenov@abv.bg>
2023-09-27 10:58:46 +0200

    Create SoulMaster.java

---
+++ InheritanceExercises/PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,9 @@
+package InheritanceExercises03PlayersAndMonsters;
+
+public class SoulMaster extends DarkWizard{
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+
+
+}
