dee5d86bdcddb1dbdc083501fbde49eabfd6d0e2
IvanRusenov <i_rusenov@abv.bg>
2023-09-27 10:58:14 +0200

    Create Main.java

---
+++ InheritanceExercises/PlayersAndMonsters/Main.java
@@ -0,0 +1,10 @@
+package InheritanceExercises03PlayersAndMonsters;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BladeKnight bladeKnight = new BladeKnight("jhjh", 105);
+        System.out.println(bladeKnight.toString());
+
+    }
+}
