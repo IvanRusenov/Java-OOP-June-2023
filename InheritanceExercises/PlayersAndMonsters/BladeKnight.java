572034a269733b0ee1a9df6bde8d1449c45845f3
IvanRusenov <i_rusenov@abv.bg>
2023-09-27 10:56:37 +0200

    Create BladeKnight.java

---
+++ InheritanceExercises/PlayersAndMonsters/BladeKnight.java
@@ -0,0 +1,9 @@
+package InheritanceExercises03PlayersAndMonsters;
+
+public class BladeKnight extends DarkKnight{
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+
+
+}
