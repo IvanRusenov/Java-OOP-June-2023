dcd31712d2c01684da37a6780c9bbebf4451b807
IvanRusenov <i_rusenov@abv.bg>
2023-09-29 10:24:38 +0200

    Create Dessert.java

---
+++ InheritanceExercises/Restaurant/Dessert.java
@@ -0,0 +1,15 @@
+package InheritanceExercises05Restaurant;
+
+import java.math.BigDecimal;
+
+public class Dessert extends Food {
+    private double calories;
+    public Dessert(String name, BigDecimal price, double grams, double calories) {
+        super(name, price, grams);
+        this.calories = calories;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+}
