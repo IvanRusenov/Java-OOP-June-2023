3b186cd360c3153506f68013cadfb9141a662e9f
IvanRusenov <i_rusenov@abv.bg>
2023-10-04 11:17:47 +0200

    Create Chinese.java

---
+++ InterfacesAndAbstractionLab/04SayHelloExtended/Chinese.java
@@ -0,0 +1,13 @@
+package InterfacesAndAbstractionLab04SayHelloExtended;
+
+public class Chinese extends BasePerson{
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
