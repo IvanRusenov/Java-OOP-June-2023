0bedb39bdc62cd7fc1a90a7d79ffa8789d531c10
IvanRusenov <i_rusenov@abv.bg>
2023-10-15 09:41:23 +0200

    Create Feline.java

---
+++ PolymorphismLab/04WildFarm/Feline.java
@@ -0,0 +1,9 @@
+package PolymorphismLab04WildFarm;
+
+public abstract class Feline extends Mammal {
+
+    protected Feline(String name, String type, Double weight, String livingRegion) {
+        super(name, type, weight, livingRegion);
+    }
+
+}
