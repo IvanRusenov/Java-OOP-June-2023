20311ada81099355bbbcae160ddce1ac39b632aa
IvanRusenov <i_rusenov@abv.bg>
2023-10-20 10:20:55 +0200

    Create FemaleRobot.java

---
+++ Java OOP Regular Exam - 8 April 2023/robotService/entities/robot/FemaleRobot.java
@@ -0,0 +1,15 @@
+package robotService.entities.robot;
+
+public class FemaleRobot extends BaseRobot{
+
+    private static final int INITIAL_KG = 7;
+
+    public FemaleRobot(String name, String kind, double price) {
+        super(name, kind, INITIAL_KG, price);
+    }
+
+    @Override
+    public void eating() {
+        setKilograms(this.getKilograms() + 1);
+    }
+}
