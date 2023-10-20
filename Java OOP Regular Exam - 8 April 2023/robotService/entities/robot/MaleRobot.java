ac2b43de9550c9c44cc86681f8e8b10a4cdb9292
IvanRusenov <i_rusenov@abv.bg>
2023-10-20 10:21:09 +0200

    Create MaleRobot.java

---
+++ Java OOP Regular Exam - 8 April 2023/robotService/entities/robot/MaleRobot.java
@@ -0,0 +1,15 @@
+package robotService.entities.robot;
+
+public class MaleRobot extends BaseRobot {
+
+    private static final int INITIAL_KG = 9;
+
+    public MaleRobot(String name, String kind, double price) {
+        super(name, kind, INITIAL_KG, price);
+    }
+
+    @Override
+    public void eating() {
+        setKilograms(this.getKilograms() + 3);
+    }
+}
