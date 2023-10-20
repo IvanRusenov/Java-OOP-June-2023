ec9c3978313a18769282ddce39b0f9e67aab2743
IvanRusenov <i_rusenov@abv.bg>
2023-10-20 10:22:25 +0200

    Create SecondaryService.java

---
+++ Java OOP Regular Exam - 8 April 2023/robotService/entities/services/SecondaryService.java
@@ -0,0 +1,9 @@
+package robotService.entities.services;
+
+public class SecondaryService extends BaseService{
+    private static final int INITIAL_CAPACITY = 15;
+    public SecondaryService(String name) {
+        super(name, INITIAL_CAPACITY);
+    }
+
+}
