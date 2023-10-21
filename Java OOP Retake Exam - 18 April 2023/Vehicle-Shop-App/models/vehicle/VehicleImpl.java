120d0b23394236bf4b52e9eac811fe3f10aa6765
IvanRusenov <i_rusenov@abv.bg>
2023-10-21 10:03:53 +0200

    Create VehicleImpl.java

---
+++ Java OOP Retake Exam - 18 April 2023/Vehicle-Shop-App/models/vehicle/VehicleImpl.java
@@ -0,0 +1,54 @@
+package vehicleShop.models.vehicle;
+
+import static vehicleShop.common.ExceptionMessages.VEHICLE_NAME_NULL_OR_EMPTY;
+import static vehicleShop.common.ExceptionMessages.VEHICLE_STRENGTH_LESS_THAN_ZERO;
+
+public class VehicleImpl implements Vehicle {
+
+    private String name;
+    private int strengthRequired;
+
+    public VehicleImpl(String name, int strengthRequired) {
+        setName(name);
+        setStrengthRequired(strengthRequired);
+    }
+
+    public void setName(String name) {
+
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(VEHICLE_NAME_NULL_OR_EMPTY);
+        }
+        this.name = name;
+    }
+
+    public void setStrengthRequired(int strengthRequired) {
+
+        if (strengthRequired < 0) {
+            throw new IllegalArgumentException(VEHICLE_STRENGTH_LESS_THAN_ZERO);
+        }
+        this.strengthRequired = strengthRequired;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getStrengthRequired() {
+        return this.strengthRequired;
+    }
+
+    @Override
+    public boolean reached() {
+        return getStrengthRequired() == 0;
+    }
+
+    @Override
+    public void making() {
+        setStrengthRequired(getStrengthRequired() - 5);
+        if (strengthRequired <= 0) {
+            setStrengthRequired(0);
+        }
+    }
+}
