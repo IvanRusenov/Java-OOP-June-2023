ca9b3afc9675a58ad3fb49c9b25826a003b8d4cd
IvanRusenov <i_rusenov@abv.bg>
2023-10-23 16:35:24 +0200

    Create RedMagic.java

---
+++ Java OOP Retake Exam - 19 December 2022/magicGame/models/magics/RedMagic.java
@@ -0,0 +1,19 @@
+package magicGame.models.magics;
+
+public class RedMagic extends MagicImpl {
+
+
+    public RedMagic(String name, int bulletsCount) {
+        super(name, bulletsCount);
+    }
+
+    @Override
+    public int fire() {
+        if (getBulletsCount() - 1 <= 0) {
+            return 0;
+        }
+
+        setBulletsCount(getBulletsCount() - 1);
+        return getBulletsCount();
+    }
+}
