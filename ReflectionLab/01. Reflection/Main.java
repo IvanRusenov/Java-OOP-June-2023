4e9f46e88778f1b5718327bef63f668a6ab5f985
IvanRusenov <i_rusenov@abv.bg>
2023-11-02 09:19:01 +0200

    Create Main.java

---
+++ ReflectionLab/01. Reflection/Main.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Class<Reflection> reflectionClass = Reflection.class;
+
+        System.out.println(reflectionClass);
+
+//        System.out.println(reflectionClass.getName());
+        System.out.println(reflectionClass.getSuperclass());
+        Class<?>[] interfaces = reflectionClass.getInterfaces();
+        Arrays.stream(interfaces).forEach(System.out::println);
+
+        Reflection reflection = reflectionClass.getDeclaredConstructor().newInstance();
+        System.out.println(reflection);
+
+    }
+
+}
